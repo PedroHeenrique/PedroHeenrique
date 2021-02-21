3f7af77b380ee86a8b72f711266bbd0fa515153b
Pedro H S Ferreira <pedroheenri_q@hotmail.com>
2021-02-21 09:42:49 -0300

    Alterada algumas dependencias na tentativa de rodas o projeto

--- livraria-virtual/src/main/java/webService/livraria_virtual/LivroExistenteException.java
+++ livraria-virtual/src/main/java/webService/livraria_virtual/LivroExistenteException.java
@@ -2,4 +2,9 @@ package webService.livraria_virtual;
 
 public class LivroExistenteException extends RuntimeException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 }
--- livraria-virtual/src/main/java/webService/livraria_virtual/LivroNaoEncontradoException.java
+++ livraria-virtual/src/main/java/webService/livraria_virtual/LivroNaoEncontradoException.java
@@ -2,4 +2,9 @@ package webService.livraria_virtual;
 
 public class LivroNaoEncontradoException extends RuntimeException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 12L;
+
 }
